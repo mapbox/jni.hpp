a325291699202ea942930d4ca9121eb26e4b2f45
xezon <pascalkross@gmail.com>
2017-09-28 18:52:59 +0200

    Add additional type aliases in field and method classes

--- include/jni/static_method.hpp
+++ include/jni/static_method.hpp
@@ -19,6 +19,8 @@ namespace jni
 
         public:
             using TagType = TheTag;
+            using MethodType = R (Args...);
+            using ReturnType = R;
 
             StaticMethod(JNIEnv& env, const Class<TagType>& clazz, const char* name)
               : method(GetStaticMethodID(env, clazz, name, TypeSignature<R (Args...)>()()))
